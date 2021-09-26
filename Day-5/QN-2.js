3f882370d1dec0126319da759322d9a50a251e0d
abulhissam09 <mohamed.abul@fourkites.com>
2021-09-26 19:16:13 +0530

    QN-2 remaining task

--- Day-5/QN-2.js
+++ Day-5/QN-2.js
@@ -119,3 +119,127 @@ function getDistance(x1, y1, x2, y2) {
   return Math.sqrt(temp);
 }
 console.log(getDistance(100, 100, 400, 300));
+
+function getNthElement(arr, n) {
+  return arr[n - 1];
+}
+console.log(getNthElement([5, 4, 6, 7, 9], 2));
+
+function getNthElement(arr, n) {
+  if (n < 0 || n > arr.length) return -1;
+  return arr[n - 1];
+}
+console.log(getNthElement([5, 4, 6, 7, 9], 1));
+
+function getLastElement(arr) {
+  return arr[arr.length - 1];
+}
+console.log(getLastElement([5, 4, 6, 7, 9]));
+
+var obj = {
+  mykey: "dfgh",
+};
+function getProperty(obj, key) {
+  if (obj[key] === undefined) return "NA";
+  return obj[key];
+}
+console.log(getProperty(obj, "dummykey"));
+
+var obj = {};
+function addProperty(obj, key) {
+  obj[key] = true;
+  return obj;
+}
+console.log(addProperty(obj, "mykey"));
+
+var obj = {};
+function removeProperty(obj, key) {
+  delete obj[key];
+  return obj;
+}
+console.log(removeProperty(obj, "mykey"));
+
+var ar = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+function getPositives(ar) {
+  let positives = [];
+  for (let i = 0; i < ar.length; i++) {
+    if (ar[i] >= 0) positives.push(ar[i]);
+  }
+  return positives;
+}
+console.log(getPositives(ar));
+
+function powersOfTwo(n) {
+  let output = [];
+  for (let i = 0; i <= n; i++) {
+    output.push(Math.pow(2, i));
+  }
+  return output;
+}
+console.log(powersOfTwo(2));
+
+function findMax(arr) {
+  return arr.sort()[arr.length - 1];
+}
+var ar = [-5, 10, -3, 12, -9, 5, 90, 0, 1];
+console.log(findMax(ar));
+
+function printPrimes(n) {
+  let i = 1;
+  while (i <= n) {
+    if (isPrime(i)) console.log(i);
+    i++;
+  }
+}
+function isPrime(i) {
+  if (i === 1) return true;
+  for (var j = 2; j <= i; j++) {
+    if (i % j === 0) break;
+  }
+  if (j === i) return true;
+  return false;
+}
+printPrimes(100);
+
+function getPrimes(n, start) {
+  let i = start;
+  while (i <= start + n) {
+    if (isPrime(i)) console.log(i);
+    i++;
+  }
+}
+function isPrime(i) {
+  if (i === 1) return true;
+  for (var j = 2; j <= i; j++) {
+    if (i % j === 0) break;
+  }
+  if (j === i) return true;
+  return false;
+}
+getPrimes(10, 100);
+
+function reverseString(s) {
+  return s.split("").reverse().join("");
+}
+var s = reverseString("JavaScript");
+console.log(s);
+
+function mergeArrays(ar1, ar2) {
+  var result = [];
+  result = ar1.concat(ar2);
+  return result;
+}
+var ar1 = [1, 2, 3];
+var ar2 = [4, 5, 6];
+var ar = mergeArrays(ar1, ar2);
+console.log(ar);
+
+function sumCSV(s) {
+  let arr = s.split(",").map(Number);
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+console.log(sumCSV(`1.5, 2.3, 3.1, 4, 5.5, 6, 7, 8, 9, 10.9`));
