786e791757397ffd5f2f14ba6145f2b7458a3d42
AbulHissam <abulhissam009@gmail.com>
2021-10-04 11:00:36 +0530

    test commit

--- Day-5/QN-3.js
+++ Day-5/QN-3.js
@@ -6,7 +6,7 @@
 // Return all the palindromes in an array
 
 // a.Print odd numbers in an array
-// Arrow function
+// // Arrow function
 let result = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 === 1) console.log(arr[i]);
